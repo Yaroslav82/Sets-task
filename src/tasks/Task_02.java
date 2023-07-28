c2adfca86df3208fa5ea444c63816181333c884f
Yaroslav82 <yargay230607@gmail.com>
2023-07-28 22:39:19 +0300

    Update main method

--- src/tasks/Task_02.java
+++ src/tasks/Task_02.java
@@ -1,21 +1,15 @@
 package tasks;
 
-import service.DataService;
-import utils.Printer;
-
 import java.util.Set;
 
-public class Task_02 {
+public class Task_02 extends Task {
 
     public static void main(String[] args) {
-        Set<String> data = DataService.getData();
-        Printer<String> printer = new Printer<>();
-        printer.print("Initial data", data);
-        updateData(data);
-        printer.print("Updated data", data);
+        Task task = new Task_02();
+        task.start();
     }
 
-    private static void updateData(Set<String> data) {
+    public void updateData(Set<String> data) {
         data.remove("kiwi");
     }
 }
