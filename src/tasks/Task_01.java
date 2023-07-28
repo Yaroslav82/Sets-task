73cd901bb62d633acb51e151e1d43163c30d6119
Yaroslav82 <yargay230607@gmail.com>
2023-07-28 22:04:26 +0300

    Add update method

--- src/tasks/Task_01.java
+++ src/tasks/Task_01.java
@@ -5,6 +5,10 @@ import java.util.Set;
 
 public class Task_01 {
 
+    private static void updateData(Set<String> data) {
+        data.add("grape");
+    }
+
     private static HashSet<String> getData() {
         return new HashSet<>(Set.of("orange", "apple", "kiwi", "strawberry", "mango"));
     }
