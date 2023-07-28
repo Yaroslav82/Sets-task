1d9e735210981b23f0e16cb1b763206b49d8544f
Yaroslav82 <yargay230607@gmail.com>
2023-07-28 22:15:57 +0300

    Add main method

--- src/tasks/Task_01.java
+++ src/tasks/Task_01.java
@@ -1,9 +1,19 @@
 package tasks;
 
+import utils.Printer;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class Task_01 {
+
+    public static void main(String[] args) {
+        Set<String> data = getData();
+        Printer<String> printer = new Printer<>();
+        printer.print("Initial data", data);
+        updateData(data);
+        printer.print("Updated data", data);
+    }
 
     private static void updateData(Set<String> data) {
         data.add("grape");
