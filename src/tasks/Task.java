284d35633f2a2cc8c2278f816b6a273a5f9158e4
Yaroslav82 <yargay230607@gmail.com>
2023-07-28 22:38:21 +0300

    Create parent for Task_01 & Task_02

---
+++ src/tasks/Task.java
@@ -0,0 +1,17 @@
+package tasks;
+
+import service.DataService;
+import utils.Printer;
+
+import java.util.Set;
+
+public abstract class Task implements DataUpdater {
+
+    public void start() {
+        Set<String> data = DataService.getData();
+        Printer<String> printer = new Printer<>();
+        printer.print("Initial data", data);
+        updateData(data);
+        printer.print("Updated data", data);
+    }
+}
