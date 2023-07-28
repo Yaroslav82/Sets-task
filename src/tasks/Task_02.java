62a0dcc06bf1c6159a22e7f7d61cc675a3aada8c
Yaroslav82 <yargay230607@gmail.com>
2023-07-28 22:18:50 +0300

    Add getData method

---
+++ src/tasks/Task_02.java
@@ -0,0 +1,11 @@
+package tasks;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task_02 {
+
+    private static HashSet<String> getData() {
+        return new HashSet<>(Set.of("orange", "apple", "kiwi", "strawberry", "mango"));
+    }
+}
