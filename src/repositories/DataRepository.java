54829133e8fd100f195504dc458a0b7caa273755
Yaroslav82 <yargay230607@gmail.com>
2023-07-29 13:50:56 +0300

    Rename DataProvider into DataRepository

--- src/repositories/DataRepository.java
+++ src/repositories/DataRepository.java
@@ -1,9 +1,9 @@
-package service;
+package repositories;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class DataProvider {
+public class DataRepository {
 
     public static HashSet<String> getData() {
         return new HashSet<>(Set.of("orange", "apple", "kiwi", "strawberry", "mango"));
--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -1,6 +1,6 @@
 package tasks;
 
-import service.DataProvider;
+import repositories.DataRepository;
 import utils.Printer;
 
 import java.util.Set;
@@ -8,7 +8,7 @@ import java.util.Set;
 public abstract class Task implements DataUpdater {
 
     public void start() {
-        Set<String> data = DataProvider.getData();
+        Set<String> data = DataRepository.getData();
         Printer<String> printer = new Printer<>();
         printer.print("Initial data", data);
         updateData(data);
