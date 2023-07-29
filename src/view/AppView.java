2a2f6e79b029f13859d0095ee044d34c3006d30e
Yaroslav82 <yargay230607@gmail.com>
2023-07-29 12:59:20 +0300

    Create view

---
+++ src/view/AppView.java
@@ -0,0 +1,14 @@
+package view;
+
+public class AppView {
+
+    private void showMenu() {
+        System.out.print("""
+                
+                ______ MENU _______
+                1 - Show task 1.
+                2 - Show task 2.
+                0 - Close the App.
+                """);
+    }
+}
