afc009d92c3c8eafa930d1755230cefb062c8612
Yaroslav82 <yargay230607@gmail.com>
2023-07-29 13:00:55 +0300

    Move main method in App

--- src/tasks/Task_01.java
+++ src/tasks/Task_01.java
@@ -4,11 +4,6 @@ import java.util.Set;
 
 public class Task_01 extends Task {
 
-    public static void main(String[] args) {
-        Task task = new Task_01();
-        task.start();
-    }
-
     public void updateData(Set<String> data) {
         data.add("grape");
     }
--- src/tasks/Task_02.java
+++ src/tasks/Task_02.java
@@ -4,11 +4,6 @@ import java.util.Set;
 
 public class Task_02 extends Task {
 
-    public static void main(String[] args) {
-        Task task = new Task_02();
-        task.start();
-    }
-
     public void updateData(Set<String> data) {
         // В методі removeIf ми проходимося циклом по всьому Set.
         // Тому за умови el.equals("kiwi") ми спочатку перевірямо
